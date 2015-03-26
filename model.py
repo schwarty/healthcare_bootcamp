d19c882a2f5da700a726d9ba3406f1673a5a45e4
Yannick Schwartz <yannick.schwartz@gmail.com>
2015-03-26 10:50:27 +0100

    up

--- model.py
+++ model.py
@@ -1,12 +1,18 @@
+import numpy as np
+
 from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.base import TransformerMixin
 from sklearn import clone
 
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import StandardScaler
-from sklearn.feature_selection import SelectPercentile, f_classif
-from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import SelectPercentile, f_classif, RFECV
+from sklearn.linear_model import LogisticRegression, RidgeClassifierCV
 from sklearn.pipeline import Pipeline
+from sklearn.externals.joblib import Parallel, delayed
+from sklearn.ensemble import BaggingClassifier
+from sklearn.cross_validation import cross_val_score, StratifiedShuffleSplit
 
 
 
@@ -16,43 +22,35 @@ def model(X_train, y_train, X_test):
     y_pred = clf.predict(X_test)
     y_score = clf.predict_proba(X_test)
     return y_pred, y_score
-
+        
 
 def model_spec():
-    class BlendingEnsemble(BaseEstimator, ClassifierMixin):
-
-        def __init__(self, estimators, blender=None):
-            self.estimators = estimators
-            self.blender = blender
-
-        def fit(self, X, y):
-            self.classes_ = np.unique(y)
-            self.estimators_ = [clone(e).fit(X, y) for e in self.estimators]
-            new_features = np.hstack([e.predict_proba(X)[:, 1:] for e in self.estimators_])
-            blender = LogisticRegression(C=1) if self.blender is None else self.blender
-            self.blender_ = clone(blender).fit(new_features, y)
-            return self
-
-        def predict_proba(self, X):
-            new_features = np.hstack([e.predict_proba(X)[:, 1:] for e in self.estimators_])
-            return self.blender_.predict_proba(new_features)
-
-        def predict(self, X):
-            proba = self.predict_proba(X)
-            return self.classes_.take(np.argmax(proba, axis=1), axis=0)
-
-
-        clf1 = Pipeline([('imputer', Imputer(strategy='most_frequent')),
+        return Pipeline([('imputer', Imputer(strategy='most_frequent')),
                          ('scaler', StandardScaler()),
-                         ('select', SelectPercentile(f_classif, 90)),
-                         ('clf', AdaBoostClassifier(RandomForestClassifier(n_estimators=300, max_depth=3, n_jobs=-1), n_estimators=20)),
-])
+                         ('select', SelectPercentile(f_classif, 85)),
+                         ('clf', BaggingClassifier(LogisticRegression(C=.01, penalty='l2'), n_estimators=100,
+                                                   bootstrap_features=True, n_jobs=-1)),
+                      ])
 
-        clf2 = Pipeline([('imputer', Imputer(strategy='most_frequent')),
+
+if __name__ == '__main__':
+    import pandas as pd
+
+    df = pd.read_csv('train.csv')
+    df.head()
+    print df.shape
+
+    y = df['TARGET'].values
+    X = df.drop('TARGET', axis=1).values
+
+    clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
                          ('scaler', StandardScaler()),
-                         ('select', SelectPercentile(f_classif, 50)),
-                         ('clf', LogisticRegression(C=.01, penalty='l2')),
-])
+                         ('select1', SelectPercentile(f_classif, 60)),
+                         ('select2', RFECV(LogisticRegression(C=.01, penalty='l2', class_weight='auto'), scoring='roc_auc')),
+                         ('clf', LogisticRegression(C=.01, penalty='l2', class_weight='auto')),
+                      ])
 
-        clf = BlendingEnsemble([clf1, clf2])
-        return clf
+    cv = StratifiedShuffleSplit(y, n_iter=5, random_state=0)
+
+    scores = cross_val_score(clf, X, y, cv=cv, scoring='roc_auc', n_jobs=-1)
+    print np.mean(scores)
