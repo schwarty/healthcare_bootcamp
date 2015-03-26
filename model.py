bada7b6f3c5251233e45f98dc1e589b7bf57e94b
Yannick Schwartz <yannick.schwartz@gmail.com>
2015-03-26 14:55:32 +0100

    rebla

--- model.py
+++ model.py
@@ -11,23 +11,19 @@ from sklearn import clone
 from sklearn.grid_search import GridSearchCV
 
 
-class RandomForestAndLogistic(BaseEstimator, ClassifierMixin):
+class MeetForester(BaseEstimator, ClassifierMixin):
 
     def __init__(self, C=1., penalty='l2', percentile=60, n_estimators=100, n_jobs=1, random_state=None):
-        self.C = C
-        self.penalty = penalty
         self.percentile = percentile
         self.n_estimators = n_estimators
         self.n_jobs = n_jobs
         self.random_state = random_state
 
         self.base_estimator = Pipeline([
-            # ('imputer_mean', Imputer(strategy='mean')),
             ('imputer_most_frequent', Imputer(strategy='most_frequent')),
             ('select', SelectPercentile(f_classif, percentile)),
-            ('rf', RandomForestClassifier(n_estimators=n_estimators, n_jobs=n_jobs, random_state=random_state)),
             ('scaler', StandardScaler()),
-            ('clf', LogisticRegression(C=C, penalty=penalty, class_weight='auto', random_state=random_state)),
+            ('rf', RandomForestClassifier(n_estimators=n_estimators, n_jobs=n_jobs, random_state=random_state)),
         ])
 
     def fit(self, X, y):
@@ -40,6 +36,31 @@ class RandomForestAndLogistic(BaseEstimator, ClassifierMixin):
     def predict_proba(self, X):
         return self.estimator_.predict_proba(X)
 
+
+class MeetLogistic(BaseEstimator, ClassifierMixin):
+
+    def __init__(self, C=1., penalty='l2', percentile=60, random_state=None):
+        self.C = C
+        self.penalty = penalty
+        self.percentile = percentile
+        self.random_state = random_state
+
+        self.base_estimator = Pipeline([
+            ('imputer_most_frequent', Imputer(strategy='most_frequent')),
+            ('scaler', StandardScaler()),
+            ('select', SelectPercentile(f_classif, percentile)),
+            ('clf', LogisticRegression(C=C, penalty=penalty, class_weight='auto', random_state=random_state)),
+        ])
+
+    def fit(self, X, y):
+        self.estimator_ = clone(self.base_estimator).fit(X, y)
+        return self
+
+    def predict(self, X):
+        return self.estimator_.predict(X)
+
+    def predict_proba(self, X):
+        return self.estimator_.predict_proba(X)
 
 
 class AveragingEnsemble(BaseEstimator, ClassifierMixin):
@@ -71,18 +92,21 @@ if __name__ == '__main__':
 
     random_state = 0
 
-    clf = RandomForestAndLogistic(random_state=random_state)
+    rf = MeetForester(random_state=random_state)
+    lr = MeetLogistic(random_state=random_state)
+
+    clf = AveragingEnsemble([rf, lr])
     cv = StratifiedShuffleSplit(y, n_iter=5, random_state=random_state)
 
-    param_grid = {
-        'C': np.logspace(-2, 2, 5),
-        'penalty': ['l1', 'l2'],
-        'n_estimators': [50, 100, 300],
-        'percentile': [50, 60, 80],
-        }
+    # param_grid = {
+    #     'C': np.logspace(-2, 2, 5),
+    #     'penalty': ['l1', 'l2'],
+    #     'n_estimators': [50, 100, 300],
+    #     'percentile': [50, 60, 80],
+    #     }
 
-    grid = GridSearchCV(clf, param_grid=param_grid, scoring='roc_auc', n_jobs=-1)
-    grid.fit(X, y)
+    # grid = GridSearchCV(clf, param_grid=param_grid, scoring='roc_auc', n_jobs=-1)
+    # grid.fit(X, y)
 
-    scores = cross_val_score(grid, X, y, cv=cv, scoring='roc_auc')
+    scores = cross_val_score(clf, X, y, cv=cv, scoring='roc_auc')
     print np.mean(scores)
